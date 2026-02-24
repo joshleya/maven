a0aa7684d336ac767e5edc4b7ef6a774bc782271
Leya <joshlinmariyaleya.27csa@licet.ac.in>
2026-02-24 11:00:12 +0530

    First commit

---
+++ src/main/java/com/example/demo/StudentController.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class StudentController {
+
+    @GetMapping("/form")
+    public String showForm(Model model) {
+        model.addAttribute("student", new Student());
+        return "form";
+    }
+
+    @PostMapping("/success")
+    public String showSuccess(@ModelAttribute Student student, Model model) {
+        model.addAttribute("student", student);
+        return "success";
+    }
+}
